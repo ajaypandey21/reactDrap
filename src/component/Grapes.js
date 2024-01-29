6e98958f7ff2b9cf0549e752b37f484bd8b23610
Ajay <ajay@drapcode.com>
2024-01-29 16:03:39 +0530

    tried class component but didnt work rolled back

--- src/component/Grapes.js
+++ src/component/Grapes.js
@@ -1,32 +1,11 @@
-import React, { Component } from "react";
-import grapesjs from "grapesjs";
-import "grapesjs/dist/css/grapes.min.css";
+import React, { Component } from 'react'
 
-class GrapesEditor extends Component {
-  componentDidMount() {
-    // Initialize GrapesJS editor
-    this.editor = grapesjs.init({
-      container: "#gjs",
-      components: '<div class="my-component">My Custom Component</div>', // Add custom component
-      plugins: ["gjs-blocks-basic"], // Load basic blocks plugin
-      pluginsOpts: {
-        "gjs-blocks-basic": {}, // Basic blocks plugin options
-      }, // Specify the container where the editor will be rendered
-    });
-
-    // You can also define custom components, plugins, etc. here
-  }
-
-  componentWillUnmount() {
-    // Cleanup on component unmount
-    if (this.editor) {
-      this.editor.destroy();
-    }
-  }
-
+export default class GrapesEditor extends Component {
   render() {
-    return <div id="gjs" />;
+    return (
+      <div>
+        this is class Component        
+      </div>
+    )
   }
 }
-
-export default GrapesEditor;
